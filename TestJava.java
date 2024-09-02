6a092d8a8d0b501be0af1e013a3cd286addc7a43
lichenxing <664940939@qq.com>
2024-09-02 20:50:45 +0800

    20240902-javasetest，配置浩鲸编码助手工具，测试浩鲸编码助手工具

---
+++ TestJava.java
@@ -0,0 +1,9 @@
+public class TestJava {
+    public static void main(String[] args) {
+    // System.out.println("Hello, World!");
+        hello();
+    }
+    private static void hello() {
+        System.out.println("Hello, World!");
+    }
+}
